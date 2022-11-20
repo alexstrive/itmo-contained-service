cc948fec8e2593ed0f1049f340ca49c53e29ded2
Alex Strive <alexstrive@pm.me>
2022-11-20 21:36:52 +0300

    add counter with postgres

--- app.go
+++ app.go
@@ -1,39 +1,101 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
+	"database/sql"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
 
-var state int
+var isDbConnected = false
+var db sql.DB
 
-func GetState() string {
-	return strconv.Itoa(state)
+func ConnectDb() {
+	connStr := "user=postgres dbname=postgres password=example sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		println(err.Error())
+	}
+
+	isDbConnected = true
 }
 
-func IncrementState() string {
-	state++
-	return GetState()
+func GetState() int {
+	connStr := os.Getenv("DB_STR")
+	if connStr == "" {
+		connStr = "user=postgres dbname=postgres password=example sslmode=disable"
+	}
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		println(err.Error())
+	}
+
+	defer db.Close()
+
+	var count int
+	row := db.QueryRow("SELECT count(*) FROM table_Counter")
+	err = row.Scan(&count)
+	if err != nil {
+		println(err.Error())
+	}
+
+	return count
+}
+
+func IncrementState(clientInfo string) {
+	connStr := os.Getenv("DB_STR")
+	if connStr == "" {
+		connStr = "user=postgres dbname=postgres password=example sslmode=disable"
+	}
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		println(err.Error())
+	}
+
+	defer db.Close()
+
+	_, err = db.Exec("INSERT INTO table_counter(datetime, client_info) VALUES (NOW(), $1)", clientInfo)
+
+	if err != nil {
+		println(err.Error())
+	}
 }
 
 //localhost:port/ (возвращается текущее значение счётчика, инкремента не происходит);
 func HandleStateRender(c *gin.Context) {
-	c.String(http.StatusOK, GetState())
+	c.String(http.StatusOK, strconv.Itoa(GetState()))
 }
 
 //localhost:port/stat (возвращается текущее значение счётчика, и происходит инкремент);
 func HandleStatIncrement(c *gin.Context) {
-	c.String(http.StatusOK, IncrementState())
-	// or if literally:
-	//
-	// HandleStateRender(c)
-	// state++
+	var userInfo = c.Request.UserAgent()
+	IncrementState(userInfo)
+	c.String(http.StatusOK, strconv.Itoa(GetState()))
 }
 
 //localhost:port/about (возвращается html-страничка ниже) как показано в app.py, функция hello в gist только с вашим именем <h3> Hello , _your_name_</h3>.
 func HandleAboutPage(c *gin.Context) {
+
 	c.HTML(http.StatusOK, "about.tmpl", gin.H{
 		"hostname": c.Request.Host,
 		"name":     "Alexey Novopashin",
